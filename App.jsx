71b16a53d03f5303dbc702e20e4c3d762a67aa61
Horixion <sibonkosi646@gmail.com>
2026-05-04 18:45:45 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{fontFamily:'Arial', padding:'40px', textAlign:'center'}}>
+      <h1>Hope Restoration Ministries</h1>
+      <p>Restoring hope to our world</p>
+    </div>
+  )
+}
